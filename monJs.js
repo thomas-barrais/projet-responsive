e7626ae14388dd4f7b87407edd867a1110b15627
Thomas <thomas.barrais@estp.fr>
2023-04-11 01:27:46 +0200

    nice commit

--- monJs.js
+++ monJs.js
@@ -1,3 +1,14 @@
+// Envoyer l'email
+function envoyerEmail() {
+  var nom = document.getElementById("nom").value;
+  var message = document.getElementById("message").value;
+  var sujet = "Formulaire de contact";
+  var corps = "Nom: " + nom + "\nMessage: " + message;
+  var lien = "mailto:thomas.barrais@estp.fr?subject=" + encodeURIComponent(sujet) + "&body=" + encodeURIComponent(corps);
+  window.location.href = lien;
+}
+
+
 // JavaScript pour détecter quand l'objet est visible dans la fenêtre du navigateur
 window.addEventListener('scroll', function() {
   var elementCible = document.querySelector('.element-cible');
@@ -14,25 +25,15 @@ window.addEventListener('scroll', function() {
   var positionTop = elementCible.getBoundingClientRect().top;
   var windowHeight = window.innerHeight;
 
-  if (positionTop - windowHeight + 50 <= 0) { // L'objet est visible dans la fenêtre avec un décalage de 50 pixels
-    elementCible.style.animationPlayState = 'running'; // Jouer l'animation en modifiant la propriété animation-play-state
+  if (positionTop - windowHeight + 50 <= 0) { 
+    elementCible.style.animationPlayState = 'running';
   }
 });
 
-// window.addEventListener('scroll', function() {
-//   var elementCible = document.querySelector('.avis1' '.avis2' '.avis3');
-//   var positionTop = elementCible.getBoundingClientRect().top;
-//   var windowHeight = window.innerHeight;
-
-//   if (positionTop - windowHeight + 50 <= 0) { // L'objet est visible dans la fenêtre avec un décalage de 50 pixels
-//     elementCible.style.animationPlayState = 'running'; // Jouer l'animation en modifiant la propriété animation-play-state
-//   }
-// });
-
 window.addEventListener('scroll', function() {
   var elementCible1 = document.querySelector('.avis1'); // Cibler le premier élément avec la classe 'avis1'
-  var elementCible2 = document.querySelector('.avis2'); // Cibler le deuxième élément avec la classe 'avis2'
-  var elementCible3 = document.querySelector('.avis3'); // Cibler le troisième élément avec la classe 'avis3'
+  var elementCible2 = document.querySelector('.avis2');
+  var elementCible3 = document.querySelector('.avis3');
 
   var positionTop1 = elementCible1.getBoundingClientRect().top;
   var positionTop2 = elementCible2.getBoundingClientRect().top;
@@ -44,29 +45,8 @@ window.addEventListener('scroll', function() {
 
   if (maxPositionTop - windowHeight + 50 <= 0) { // Vérifier si la position la plus haute est visible dans la fenêtre avec un décalage de 50 pixels
     elementCible1.style.animationPlayState = 'running'; // Jouer l'animation pour le premier élément en modifiant la propriété animation-play-state
-    elementCible2.style.animationPlayState = 'running'; // Jouer l'animation pour le deuxième élément en modifiant la propriété animation-play-state
-    elementCible3.style.animationPlayState = 'running'; // Jouer l'animation pour le troisième élément en modifiant la propriété animation-play-state
-  }
-});
-
-
-window.addEventListener('scroll', function() {
-  var elementCible = document.querySelector('.avis2');
-  var positionTop = elementCible.getBoundingClientRect().top;
-  var windowHeight = window.innerHeight;
-
-  if (positionTop - windowHeight + 50 <= 0) { // L'objet est visible dans la fenêtre avec un décalage de 50 pixels
-    elementCible.style.animationPlayState = 'running'; // Jouer l'animation en modifiant la propriété animation-play-state
-  }
-});
-
-window.addEventListener('scroll', function() {
-  var elementCible = document.querySelector('.avis3');
-  var positionTop = elementCible.getBoundingClientRect().top;
-  var windowHeight = window.innerHeight;
-
-  if (positionTop - windowHeight + 50 <= 0) { // L'objet est visible dans la fenêtre avec un décalage de 50 pixels
-    elementCible.style.animationPlayState = 'running'; // Jouer l'animation en modifiant la propriété animation-play-state
+    elementCible2.style.animationPlayState = 'running'; 
+    elementCible3.style.animationPlayState = 'running'; 
   }
 });
 
@@ -75,8 +55,8 @@ window.addEventListener('scroll', function() {
   var positionTop = elementCible.getBoundingClientRect().top;
   var windowHeight = window.innerHeight;
 
-  if (positionTop - windowHeight + 50 <= 0) { // L'objet est visible dans la fenêtre avec un décalage de 50 pixels
-    elementCible.style.animationPlayState = 'running'; // Jouer l'animation en modifiant la propriété animation-play-state
+  if (positionTop - windowHeight + 50 <= 0) {
+    elementCible.style.animationPlayState = 'running';
   }
 });
 
@@ -85,7 +65,7 @@ window.addEventListener('scroll', function() {
   var positionTop = elementCible.getBoundingClientRect().top;
   var windowHeight = window.innerHeight;
 
-  if (positionTop - windowHeight + 50 <= 0) { // L'objet est visible dans la fenêtre avec un décalage de 50 pixels
-    elementCible.style.animationPlayState = 'running'; // Jouer l'animation en modifiant la propriété animation-play-state
+  if (positionTop - windowHeight + 50 <= 0) {
+    elementCible.style.animationPlayState = 'running';
   }
 });
